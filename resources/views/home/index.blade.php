e329b3431025a589f1bea6e4dfd04dad2158f1d8
liuyingjie <handanyingjie@sina.com>
2018-06-21 12:50:17 +0800

    删除panel_body

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,16 +6,6 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">最新文章</div>
-
-                    <div class="panel-body">
-                        @foreach($posts as $post)
-                            <article>
-                                <a href="{{ route('home_show',['post' => $post->id]) }}">
-                                    <h4>{{ $post->title }}</h4>
-                                </a>
-                            </article>
-                        @endforeach
-                    </div>
                     <ul class="list-group">
                         @foreach($posts as $post)
                             <li class="list-group-item">
